09d88db503301a2afa3bcf8f55e481c026dbdffb
JackyZheng657 <j.zheng657@hotmail.com>
2025-04-28 16:20:01 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
+import { FooterComponent } from "./core/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'current-directory';
+
 }
